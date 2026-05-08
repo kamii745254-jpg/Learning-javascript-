7dfe360ead2b3199e5040b6ce7eb8daf35b0a149
Kamishka Waheed <kamii745254@gmail.com>
2026-05-08 22:34:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// Method 1: toString()
+// Short Explanation
+
+// toString() converts a number into a string.
+// It changes number type into text type.
+
+// Simple Example
+// let price = 500;
+// console.log(price.toString());
+;
+// let number = 250;
+// console.log(number.toString());
+// ;
+// let price =500;
+// console.log(price.toString());
+
+// toExponential()
+// Short Explanation
+
+// toExponential() converts a number into exponential form (scientific notation).
+// It is useful for very large or very small numbers.
+
+// Simple Example
+// let price = 5000;
+// console.log(price.toExponential(2));
+// let rupees = 200;
+// console.log(rupees.toExponential(1));
+;
+// let num = 2000;
+// console.log(num.toExponential(1));
+;
+
+// toFixed()
+// Short Explanation
+
+// toFixed() fixes the number of digits after the decimal point.
+// It is useful for prices and money values.
+// let amount = 45.678;
+// console.log(amount.toFixed(1));
+
+// Method 3: toPrecision()
+// Short Explanation
+
+// toPrecision() formats a number to a specific total number of digits.
+// It is useful when we want exact precision in values.
+// ;
+// let price = 123.456;
+// console.log(price.toPrecision(4));
+
+;
+// valueOf()
+// Short Explanation
+
+// valueOf() returns the actual value of a number.
+// It gives the primitive number value from a Number object.
+// let marks = 500;
+// console.log(marks.valueOf());
+
+
+
+// OTHERSSSSSSSS
+
+// 1. toString()
+// let marks = 95;
+// console.log(marks.toString());
+
+// 2. toExponential()
+// let price = 5000;
+// console.log(price.toExponential(2));
+
+// // 3. toFixed()
+// let amount = 99.4567;
+// console.log(amount.toFixed(2));
+
+// // 4. toPrecision()
+// let num = 123.456;
+// console.log(num.toPrecision(4));
+
+// // 5. valueOf()
+// let value = 250;
+// console.log(value.valueOf());
+
+// // 6. Number()
+// let textNumber = "300";
+// console.log(Number(textNumber));
+
+// // 7. parseInt()
+// let decimalText = "99.99";
+// console.log(parseInt(decimalText));
+
+// // 8. parseFloat()
+// let decimalValue = "88.75";
+// console.log(parseFloat(decimalValue));
+
+// // 9. isNaN()
+// let checkValue = "Hello";
+// console.log(isNaN(checkValue));
+
+// // 10. isFinite()
+// let finalValue = 100;
+// console.log(isFinite(finalValue));
